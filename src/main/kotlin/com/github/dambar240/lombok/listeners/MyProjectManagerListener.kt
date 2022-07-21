6ab6dd2dd89791f966982c8d203182f6c43f1aec
GitHub Action <action@github.com>
2022-07-21 08:36:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/dambar240/lombok/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/dambar240/lombok/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.dambar240.lombok.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.dambar240.lombok.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/dambar240/lombok/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/dambar240/lombok/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.dambar240.lombok.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dambar240.lombok.MyBundle
 
 class MyApplicationService {
 
